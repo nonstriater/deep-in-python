e959bf1e89e622a0f91e22e2dbdad72b1d3ee46b
冉文杰 <510495266@qq.com>
2020-07-11 13:37:07 +0800

    none value

---
+++ none/test.py
@@ -0,0 +1,10 @@
+
+a = ''
+b = False
+c = []
+
+print(a==None)   #比较值
+print(b==None)
+print(c==None)
+
+print(a is None)
---
+++ none/test2.py
@@ -0,0 +1,14 @@
+
+def fun():
+    return None
+
+a =fun()
+if not a:   #逻辑运算
+    print('S')
+else:
+    print('F')
+
+if a is None:
+    print('S')
+else:
+    print('F')
